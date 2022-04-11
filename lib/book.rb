046bafbff52e0d58c27088d7e47ff79741af787b
Evan Watson <thomasevanwatson@gmail.com>
2022-04-11 10:53:09 -0400

    commit tree 061bcb1d3394a7a878ab389031faa9bb06fcc9bb

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre 
+
+    
+def initialize(title_in = "And Then There Were None")
+    @title = title_in
+    @page_count = 0
+end
+
+
+def turn_page
+   puts "Flipping the page...wow, you read fast!"
+end
+
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+
+class Shoe 
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+def initialize(brand_in = "Adidas")
+    @brand = brand_in
+    @condition = "old"
+end
+
+
+def cobble
+@condition = "new"
+puts "Your shoe is as good as new!"
+end
+
+    
+end
